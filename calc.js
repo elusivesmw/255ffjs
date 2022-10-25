b4c4e510657da98e89be7cff05e507a3873ac555
elusive <elusivesmw@gmail.com>
2022-10-25 07:32:51 -0700

    Added binary. Added 0 padding for hex and binary. Updated logger.

--- calc.js
+++ calc.js
@@ -4,9 +4,10 @@ const inputs = document.getElementsByClassName("input");
 const outputs = document.getElementsByClassName("output");
 
 const modeRadios = document.getElementsByName("modeRadio");
-const signedDecTextbox = document.getElementById("signedDecTextbox");
 const unsignedDecTextbox = document.getElementById("unsignedDecTextbox");
+const signedDecTextbox = document.getElementById("signedDecTextbox");
 const hexTextbox = document.getElementById("hexTextbox");
+const binaryTextbox = document.getElementById("binaryTextbox");
 
 const incButton = document.getElementById("incButton");
 const decButton = document.getElementById("decButton");
@@ -14,6 +15,7 @@ const decButton = document.getElementById("decButton");
 const valueSpan = document.getElementById("valueSpan");
 
 // consts
+const BIN = 2;
 const DEC = 10;
 const HEX = 16;
 
@@ -28,7 +30,8 @@ const LOG_LEVEL = Object.freeze({
     error: 1,
     warning: 3,
     info: 7,
-    verbose: 15
+    debug: 15,
+    trace: 31
 });
 
 class Logger {
@@ -61,8 +64,14 @@ class Logger {
     }
 
     debug(msg) {
-        if ((this.level & LOG_LEVEL.verbose) == LOG_LEVEL.verbose) {
+        if ((this.level & LOG_LEVEL.debug) == LOG_LEVEL.debug) {
             console.log("%c" + msg, "color: grey;");
+        }
+    }
+
+    trace(msg) {
+        if ((this.level & LOG_LEVEL.trace) == LOG_LEVEL.trace) {
+            console.log("%c" + msg, "color: white;");
         }
     }
 }
@@ -95,7 +104,13 @@ class Calc {
     }
 
     setFromHex(newValue, callback) {
-        this.value = parseInt(this.mode & newValue, HEX);
+        this.value = this.mode & parseInt(newValue, HEX);
+
+        if (callback) callback();
+    }
+
+    setFromBinary(newValue, callback) {
+        this.value = parseInt(this.mode) & parseInt(newValue, BIN);
 
         if (callback) callback();
     }
@@ -130,7 +145,20 @@ class Calc {
     }
     
     get hex() {
-        return this.value.toString(HEX);
+        let modeMaxHex = parseInt(this.mode).toString(HEX);
+        return this.zeroPad(this.value.toString(HEX), modeMaxHex.length);
+    }
+
+    get binary () {
+        let modeMaxBinary = parseInt(this.mode).toString(BIN);
+        return this.zeroPad(this.value.toString(BIN), modeMaxBinary.length);
+    }
+
+    zeroPad(value, length) {
+        while (value.length < length) {
+            value = "0" + value;
+        }
+        return value;
     }
 }
 
@@ -150,25 +178,31 @@ function updateAll(sender) {
     for (output of outputs) {
         // don't update sender to prevent infinite loop
         if (output != sender) {
-            switch (output.id) {
-                case unsignedDecTextbox.id:
-                    output.value = calc.unsignedDec;
-                    break; 
-                case signedDecTextbox.id:
-                    output.value = calc.signedDec;
-                    break; 
-                case hexTextbox.id:
-                    output.value = calc.hex;
-                    break;
-                case valueSpan.id:
-                    output.innerText = calc.value;
-                    break; 
-            }
+            updateControl(output);
         }
     }
 }
 
-
+// update specific control
+function updateControl(sender) {
+    switch (sender.id) {
+        case unsignedDecTextbox.id:
+            output.value = calc.unsignedDec;
+            break; 
+        case signedDecTextbox.id:
+            sender.value = calc.signedDec;
+            break; 
+        case hexTextbox.id:
+            sender.value = calc.hex;
+            break;
+        case binaryTextbox.id:
+            sender.value = calc.binary;
+            break;
+        case valueSpan.id:
+            sender.innerText = calc.value;
+            break; 
+    }
+}
 
 
 // add mode listeners
@@ -178,19 +212,30 @@ for (const radio of modeRadios) {
 
 // add validation listeners
 unsignedDecTextbox.addEventListener("keydown", validateUnsignedDec);
+signedDecTextbox.addEventListener("keydown", validateSignedDec);
+hexTextbox.addEventListener("keydown", validateHex);
+binaryTextbox.addEventListener("keydown", validateBinary);
 
 // add value listeners
 unsignedDecTextbox.addEventListener("input", inputChanged);
 signedDecTextbox.addEventListener("input", inputChanged);
 hexTextbox.addEventListener("input", inputChanged);
+binaryTextbox.addEventListener("input", inputChanged);
 
 // add button listeners
 incButton.addEventListener("click", incClicked)
 decButton.addEventListener("click", decClicked)
 
+// leave
+unsignedDecTextbox.addEventListener("blur", inputLeft);
+signedDecTextbox.addEventListener("blur", inputLeft);
+hexTextbox.addEventListener("blur", inputLeft);
+binaryTextbox.addEventListener("blur", inputLeft);
+
+
 // events
 function modeChanged(event) {
-    console.log("mode changed")
+    logger.info("mode changed");
 
     let selected = event.target;
     calc.setMode(selected.value);
@@ -200,7 +245,7 @@ function modeChanged(event) {
 }
 
 function inputChanged(event) {
-    console.log("input changed")
+    logger.info("input changed");
 
     switch (event.target.id) {
         case unsignedDecTextbox.id:
@@ -212,9 +257,21 @@ function inputChanged(event) {
         case hexTextbox.id:
             calc.setFromHex(event.target.value);
             break;
+        case binaryTextbox.id:
+            calc.setFromBinary(event.target.value);
+            break;
     }
 
     updateAll(event.target);
+}
+
+function inputLeft(event) {
+    logger.info("input left");
+
+    if (event.target.value == "") {
+        // fill in blanks on leave
+        updateControl(event.target);
+    }
 }
 
 function incClicked() {
@@ -225,31 +282,49 @@ function decClicked() {
     calc.dec(updateAll);
 }
 
-
 function validateUnsignedDec(event) {
-    const unsignedDecFormat = new RegExp("[0-9]");
-    
+    const unsignedDecChars = new RegExp("[0-9]");
+    validateFormat(event, unsignedDecChars);
+}
+
+function validateSignedDec(event) {
+    const signedDecChars = new RegExp("[\-0-9]");
+    validateFormat(event, signedDecChars);
+}
+
+function validateHex(event) {
+    logger.error("not yet implemented");
+}
+
+function validateBinary(event) {
+    const binaryChars = new RegExp("[01]");
+    validateFormat(event, binaryChars);
+}
+
+function validateFormat(event, format) {
     if (event.ctrlKey || event.altKey || event.key.length !== 1) return;
 
-
-    if (!unsignedDecFormat.test(event.key)) {
+    if (!format.test(event.key)) {
         logger.debug(event.key + " prevented");
         event.preventDefault();
     }
+
+    // TODO: figure out this validation
+    // if (format.test(event.target.value)) {
+    //     logger.trace("whole format mismatch");
+    // }
 }
 
-function validateFormat(input, format) {
-
-}
-
-var logger = new Logger(LOG_LEVEL.verbose)
+var logger = new Logger(LOG_LEVEL.trace);
 
 logger.error("errors only");
 logger.warning("warnings and errors on");
 logger.info("info, warnings, and errors on");
-logger.debug("verbose on");
+logger.debug("debug on");
+logger.trace("trace on");
 
 // set initial mode
 // later pull from settings
 var calc = new Calc();
+updateAll();
 calc.setMode(MODE._8bit, updateMode);
